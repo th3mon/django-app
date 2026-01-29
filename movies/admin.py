dff2a3e697134c2872d3ccbba31573315bd649c1
th3mon <p.szelenberger@gmail.com>
2026-01-29 07:52:16 +0100

    Register ModelAdmins

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Genre, Movie
 
-# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ("date_created",)
+    list_display = ("name", "number_in_stock", "daily_rate")
