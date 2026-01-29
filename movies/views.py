4b67f94c59f615346f6f4898c74e8c30bf73954d
th3mon <p.szelenberger@gmail.com>
2026-01-29 15:25:02 +0100

    Add movie/detail template

--- movies/views.py
+++ movies/views.py
@@ -7,3 +7,9 @@ def index(request):
     movies = Movie.objects.all()
 
     return render(request, "movies/index.html", {"movies": movies})
+
+
+def detail(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+
+    return render(request, "movies/detail.html", {"movie": movie})
